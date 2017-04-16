0c1eb765fd33cebdb978b868586058c7c5df3474
Audrey Labuxiere <audreylabuxiere@gmail.com>
2017-04-16 13:18:40 +0200

    create, edit and delete projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
@@ -9,23 +9,45 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @project.update(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
 
   def set_project
     @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(
+    :name,
+    :short_description,
+    :summary)
   end
 end
