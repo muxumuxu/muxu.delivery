416909ca7968a123f463821415886682e797dc90
Audrey Labuxiere <audreylabuxiere@gmail.com>
2017-04-16 20:51:29 +0200

    added validations

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,3 +1,6 @@
 class Milestone < ApplicationRecord
   belongs_to :project
+  validates :name, presence: true
+  validates :status, presence: true
+  validates :delivery_date, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   has_many :milestones, dependent: :destroy
+  validates :name, presence: true
+  validates :short_description, presence: true
 end
